b412dd8551896677be7342be964b6bd135f4c438
Baural <alzhanovbb@gmail.com>
2017-03-14 01:11:23 +0600

    CodeReview.java

---
+++ CodeReview.java
@@ -0,0 +1,31 @@
+public class CodeReview {
+	public static Integer Parse(String[] Object) {
+		int o = Integer.parseInt(o.toString());
+		return 0;
+	}
+
+		/**
+		* <summary> This method returns integer parsed from the source.
+		* If parsing fails it should return zero. </summary>
+		* <param> name="o" Object with data to parse. </param>
+		* <returns>Parsed value</returns>
+		*/
+
+
+
+		/**
+		* <summary> This method returns concatenated string from the array of the strings. </summary>
+		* <param> name="lines"Array of strings with lines.</param>
+		*  <returns>Concatenated string.</returns>
+		*/
+
+	public static String Concat(String[] lines){
+	String result[]= " ";
+		for (int i = 0; i < lines.length; i++){
+			result += lines[i].length;
+	}
+	return result;
+	}
+
+
+}
