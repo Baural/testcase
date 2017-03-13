48e6f8d66847aefe73ba303685e3a8b29c3a554b
Baural <alzhanovbb@gmail.com>
2017-03-14 01:03:15 +0600

    first commit

---
+++ Analysis.java
@@ -0,0 +1,16 @@
+
+import java.util.Math;
+public class Analysis{
+	public static void main(String args[]){
+		public double GetRandomNumber1(){
+		Random random = new​ Random();
+		return Round((random.NextDouble() - 0.5) * 2);
+                }
+                public double GetRandomNumber2(){
+		Random random = new​ Random();
+                return Round((random.NextDouble() - 0.5) * 2.99);
+                }
+
+	}
+
+}
